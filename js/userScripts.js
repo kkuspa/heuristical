43c874fbab9efe4f6ef611b12e7e415a7e0a6446
Kevin Chavez <kjchavez@stanford.edu>
2014-01-24 08:58:04 -0800

    signup form connected to Parse

---
+++ js/userScripts.js
@@ -0,0 +1,29 @@
+function SignUp() {
+	var user = new Parse.User();
+	var form = document.getElementById("signupForm");
+
+	var firstname = form.elements[0].value;
+	var lastname = form.elements[1].value;
+	var password = form.elements[3].value;
+	var email = form.elements[2].value;
+	user.set("username", email);
+	user.set("password", password);
+	user.set("email", email);
+	 
+	// other fields can be set just like with Parse.Object
+	user.set("first name", firstname);
+	user.set("last name", lastname);
+	user.set("full name", firstname + " " + lastname);
+	 
+	user.signUp(null, {
+	  success: function(user) {
+		// Hooray! Let them use the app now.
+		alert("Thank you for signing up. We'll keep you updated!");
+	  },
+	  error: function(user, error) {
+		// Show the error message somewhere and let the user try again.
+		alert("Error: " + error.code + " " + error.message);
+	  }
+
+	});
+}
